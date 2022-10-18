d14b8d0c4a5b7927f6ef2af3f7f9726f04c5e816
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-10-18 07:41:56 -0500

    deleted double result in chess game

--- components/chess-game/GameButtons/index.js
+++ components/chess-game/GameButtons/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAnswerContext } from "../../../context/AnswerContext";
 import SubmitTime from "../SubmitTime";
-import styles from './GameButtons.module.css'
+import styles from "./GameButtons.module.css";
 
 const GameButtons = () => {
   const value = useAnswerContext();
@@ -16,11 +16,13 @@ const GameButtons = () => {
     <div className={styles.buttonArea}>
       {/* <button onClick={() => gameLengthClick(8)}>8</button> */}
       {/* <button onClick={() => gameLengthClick(16)}>16</button> */}
-      <button onClick={() => value.chessGameClick()} className={styles.startButton} >
+      <button
+        onClick={() => value.chessGameClick()}
+        className={styles.startButton}
+      >
         {value.count === 0 ? "start" : "stop"}
       </button>
       <div>{value.count === 0 ? "" : value.square}</div>
-      <div>{value.totalTime === 0 ? "" : value.totalTime}</div>
       <div>{value.totalTime === 0 ? "" : value.totalTime}</div>
       <div>{value.totalTime === 0 ? "" : <SubmitTime />}</div>
     </div>
