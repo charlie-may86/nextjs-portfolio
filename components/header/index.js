40a72e3e3eab9e48c865bf3391f165ee50ddf267
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-10-25 08:35:46 -0500

    testing

--- components/header/index.js
+++ components/header/index.js
@@ -30,35 +30,67 @@ const Header = () => {
           To Do
         </a>
       </div>
-      <FontAwesomeIcon icon={faBars} className={styles.navDropDown}>
-        <ul className={styles.dropDownMenu}>
-          <Link href="/cv" className={styles.dropDwonItem}>
-            CV
-          </Link>
-          <Link href="/chess_game" className={styles.dropDwonItem}>
-            Chess Game
-          </Link>
-          <Link href="/cfb_pick_em" className={styles.dropDwonItem}>
-            CFB Pickem
-          </Link>
-          <a
-            href="https://github.com/charlie-may86"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.dropDwonItem}
-          >
-            GitHub
-          </a>
-          <a
-            href="https://trello.com/b/wd7Jy3Xu/portfolio"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.dropDwonItem}
-          >
-            To Do
-          </a>
+      <div className={styles.menu}>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <ul>
+            <li>
+              <a href="#">Our Company</a>
+            </li>
+            <li>
+              <a href="#">Team</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+          </ul>
         </ul>
-      </FontAwesomeIcon>
+      </div>
+      {/* <ul className={styles.dropDownMenu}>
+        <li>
+          <FontAwesomeIcon icon={faBars} className={styles.navDropDown}>
+            <ul>
+              <li>
+                <Link href="/cv" className={styles.dropDwonItem}>
+                  CV
+                </Link>
+              </li>
+              <li>
+                <Link href="/chess_game" className={styles.dropDwonItem}>
+                  Chess Game
+                </Link>
+              </li>
+              <li>
+                <Link href="/cfb_pick_em" className={styles.dropDwonItem}>
+                  CFB Pickem
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/charlie-may86"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles.dropDwonItem}
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://trello.com/b/wd7Jy3Xu/portfolio"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles.dropDwonItem}
+                >
+                  To Do
+                </a>
+              </li>
+            </ul>
+          </FontAwesomeIcon>
+        </li>
+      </ul> */}
     </header>
   );
 };
